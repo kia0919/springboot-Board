5d5f995e81bc01194fb9ed54252a2e97de6aa079
kia0919 <john0256@naver.com>
2024-04-02 15:57:40 +0900

    2024.04.02

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.board.dto.response.board.GetLatestListResponseDto;
+import com.example.board.service.BoardService;
 
 import lombok.RequiredArgsConstructor;
 
